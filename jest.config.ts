8086abf994eb4394214cc6b669d1bed3b17dbe1c
YASH PANDEY <pandeyyash7052@gmail.com>
2025-01-24 01:53:24 +0530

    jest integration with pipelin job

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: "jest-environment-jsdom", 
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testMatch: ["<rootDir>/tests/**/*.(test|spec).(ts|tsx)"],
+  modulePaths: ["<rootDir>/src/"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: ["text", "lcov", "html", "json-summary"],
+  preset: "ts-jest",
+};
+
+export default config;
